56d06f3cb6d36a1bda382788fe51b55553956f7e
ArdillaV21 <fedevalle21@gmail.com>
2025-05-20 12:24:31 -0300

    Crear con el constructor calculadora/ pedir opcion

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,3 +1,11 @@
 ﻿using EspacioCalculadora;
 
 Calculadora calc = new Calculadora();
+string opcion = "";
+while(opcion != "x"){
+    Console.WriteLine("\n Resultado Actual: ",calc.Resultado);
+    Console.WriteLine("\n ¿Que operacion desea realizar?");
+    Console.WriteLine("[+]Sumar \n [-]Restar \n [*]Multiplicar \n [/]Dividir \n [c]Limpiar \n [x]Cerrar");
+    Console.Write("Ingrese una opcion: \n");
+    opcion = Console.ReadLine()?.ToLower() ?? "";
+}
