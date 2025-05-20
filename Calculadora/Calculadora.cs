d1ad5062d36436122a164a2b92021fd8cf0d3c9d
ArdillaV21 <fedevalle21@gmail.com>
2025-05-20 11:45:21 -0300

    class Clculadora

--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -2,23 +2,26 @@ namespace EspacioCalculadora;
 
 public class Calculadora{
     private double dato;
-    private double resultado;
-
     public double Dato { get => dato; set => dato = value; }
-    public double Resultado { get => resultado;}
     public void Sumar(double termino){
-
+        dato+=termino;
     }
     public void Restar( double termino){
-
+        dato-=termino;
     }
     public void Multiplicar(double termino){
-
+        dato*=termino;
     }
     public void Dividir(double termino){
-
+        if(termino != 0){
+            dato/=termino;
+        }else{
+            Console.WriteLine("Error: Division en 0");
+        }
     }
     public void Limpiar(){
-
+        dato = 0;
     }
+    public double Resultado => dato;
+
 };
