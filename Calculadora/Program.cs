bf362e50d588d74c259cd57856185e6549d410b8
ArdillaV21 <fedevalle21@gmail.com>
2025-05-20 12:35:09 -0300

    creacion de la ui para manejar la calculadora

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -8,4 +8,27 @@ while(opcion != "x"){
     Console.WriteLine("[+]Sumar \n [-]Restar \n [*]Multiplicar \n [/]Dividir \n [c]Limpiar \n [x]Cerrar");
     Console.Write("Ingrese una opcion: \n");
     opcion = Console.ReadLine()?.ToLower() ?? "";
-}
+
+    if( opcion== "+" || opcion == "-" || opcion == "*" || opcion == "/"){
+        Console.Write("Ingrese un numero: ");
+        if(double.TryParse(Console.ReadLine(),out double valor)){
+            switch (opcion)
+            {
+                case "+" : calc.Sumar(valor);
+                break;
+                case "-" : calc.Restar(valor);
+                break;
+                case "*" : calc.Multiplicar(valor);
+                break;
+                case "/" : calc.Dividir(valor);
+                break;
+            }
+        }else{
+            Console.WriteLine("Opcion Invalida");
+        }
+    }else if(opcion == "c"){
+        calc.Limpiar();
+    }else if(opcion != "x"){
+        Console.WriteLine("Opcion Invalida");
+    }
+    }
