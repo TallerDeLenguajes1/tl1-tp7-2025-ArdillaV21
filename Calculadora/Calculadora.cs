d90cae246a1bc63b5bd052f3dbada7a3ba80e723
ArdillaV21 <fedevalle21@gmail.com>
2025-05-20 12:18:49 -0300

    creacion de class Calculadora

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,24 @@
+namespace EspacioCalculadora;
+
+public class Calculadora{
+    private float dato;
+    private float resultado;
+
+    public float Dato { get => dato; set => dato = value; }
+    public float Resultado { get => resultado;}
+    public void Sumar(double termino){
+        
+    }
+    public void Restar( double termino){
+
+    }
+    public void Multiplicar(double termino){
+
+    }
+    public void Dividir(double termino){
+
+    }
+    public void Limpiar(){
+
+    }
+};
