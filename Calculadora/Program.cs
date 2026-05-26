e7485fb7e8dd1573e60a1f50866a576437b39361
ArdillaV21 <fedevalle21@gmail.com>
2026-05-26 12:09:18 -0300

    Crear interfaz para poder operar con la clase

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -2,5 +2,40 @@
 
 Calculadora MiCalculadora = new Calculadora();
 
-MiCalculadora.Sumar(5);
-Console.WriteLine("Resultado: " + MiCalculadora.Resultado);
+string opcion = "";
+
+while(opcion != "x")
+{
+    
+    Console.WriteLine("[+]Sumar \n [-]Restar \n [*]Multiplicar \n [/]Dividir \n [=]Resultado \n[c]Limpiar \n [x]Cerrar");
+    Console.Write("Ingrese una opcion: \n");
+    opcion = Console.ReadLine()?.ToLower() ?? "";
+
+    if( opcion== "+" || opcion == "-" || opcion == "*" || opcion == "/"){
+        Console.Write("Ingrese un numero: ");
+        if(double.TryParse(Console.ReadLine(),out double valor)){
+            switch (opcion)
+            {
+                case "+" : MiCalculadora.Sumar(valor);
+                break;
+                case "-" : MiCalculadora.Restar(valor);
+                break;
+                case "*" : MiCalculadora.Multiplicar(valor);
+                break;
+                case "/" : MiCalculadora.Dividir(valor);
+                break;
+            }
+        }else{
+            Console.WriteLine("Opcion Invalida");
+        }
+    }else if(opcion == "=")
+    {
+        Console.WriteLine("Resultado: "+ MiCalculadora.Resultado);
+    
+    }else if(opcion == "c"){
+        MiCalculadora.Limpiar();
+    }else if(opcion != "x"){
+        Console.WriteLine("Opcion Invalida");
+    }
+}
+    
