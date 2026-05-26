d7cbb5f05763571e241aa9d18ab543059bd72f97
ArdillaV21 <fedevalle21@gmail.com>
2026-05-26 11:54:09 -0300

    Modifico get set

--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -2,12 +2,6 @@ namespace EspacioCalculadora{
     public class Calculadora
     {
         private double dato;
-
-        public double Dato
-        {
-            get => dato;
-            set => dato = value;
-        }
         
         public void Sumar(double termino)
         {
