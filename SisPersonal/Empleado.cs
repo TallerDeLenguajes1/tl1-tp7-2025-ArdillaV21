0eab9bf3f225416e7375fe4636e2a473dbc48ccd
ArdillaV21 <fedevalle21@gmail.com>
2026-05-26 13:31:38 -0300

    Creacion de todos los metodos y probando program cargando un empleado

--- SisPersonal/Empleado.cs
+++ SisPersonal/Empleado.cs
@@ -1,39 +1,79 @@
 namespace EspacioEmpleado
 {
+    public enum Cargos : int
+    {
+        Auxiliar = 1,
+        Administrativo = 2,
+        Ingeniero = 3,
+        Especialista = 4,
+        Investigador = 5
+    }
+
     public class Empleado
     {
-        string Nombre;
-        string Apellido;
-        DateTime FechaDeNacimiento;
-        char EstadoCivil;
-        DateTime FechaIngreso;
+        string nombre;
+        string apellido;
+        DateTime fechaDeNacimiento;
+        char estadoCivil;
+        DateTime fechaIngreso;
 
-        double SueldoBasico;
+        double sueldoBasico;
+        Cargos cargo;
 
-        enum cargos
+        public Empleado(string nombre, string apellido, DateTime fechaDeNacimiento, char estadoCivil, DateTime fechaIngreso, double sueldoBasico, Cargos cargo)
         {
-            Auxiliar,
-            Administrativo,
-            Ingeniero,
-            Especialista,
-            Investigador
-        }
-        cargos Cargo;
-
-        public int antiguedad(DateTime FechaIngreso)
-        {
-            return DateTime.Now().Year - FechaIngreso.Year;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.fechaDeNacimiento = fechaDeNacimiento;
+            this.estadoCivil = estadoCivil;
+            this.fechaIngreso = fechaIngreso;
+            this.sueldoBasico = sueldoBasico;
+            this.cargo = cargo;
         }
 
-        public int edadEmpleado(DateTime FechaDeNacimiento)
+
+        public int antiguedad(DateTime fechaIngreso)
         {
-            return DateTime.Now().Year - FechaDeNacimiento.Year;
+            return DateTime.Now.Year - fechaIngreso.Year;
         }
 
-        public int AñosParaJubilacion(DateTime FechaDeNacimiento)
+        public int edadEmpleado(DateTime fechaDeNacimiento)
         {
-            return (65 - (DateTime.Now().Year - FechaDeNacimiento.Year));
+            return DateTime.Now.Year - fechaDeNacimiento.Year;
+        }
+
+        public int AñosParaJubilacion(DateTime fechaDeNacimiento)
+        {
+            return 65 - (DateTime.Now.Year - fechaDeNacimiento.Year);
+        }
+
+        public double Salario()
+        {
+            double Adicional = 0;
+            int Antiguedad = antiguedad(fechaIngreso);
+            if (Antiguedad >= 20)
+            {
+                Adicional = sueldoBasico * 1.25;
+            }
+            else
+            {
+                Adicional = sueldoBasico*0.1 * Antiguedad;
+            }
+
+            if(cargo == Cargos.Especialista || cargo == Cargos.Ingeniero)
+            {
+                Adicional *= 1.50;
+            }
+
+            if(estadoCivil == 'c')
+            {
+                Adicional+= 150000;
+            }
+
+            return Adicional;
         }
         
     }
+
+    
 }
--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using EspacioEmpleado;
+
+Empleado Empleado1 = new Empleado("Federico","Valle",new DateTime(2001,12,06),'s',new DateTime(2013,01,01), 250000, Cargos.Ingeniero);
+
+Console.WriteLine("Hello");
